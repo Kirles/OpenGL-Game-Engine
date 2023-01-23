bbce7f8b253b42508e0236e4d5041e4e31c51447
Kirill <k.bushtets@gmail.com>
2023-01-23 02:24:51 +0200

    Bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ extern "C" { _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001
 //extern "C" { _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1; }
 
 GLfloat point[] = {
-     0.0f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f
+     0.5f, 0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
 };
 
 GLfloat colors[] = {
@@ -26,16 +26,16 @@ const char* vertex_shader =
 "layout(location = 0) in vec3 vertex_position;"
 "layout(location = 1) in vec3 vertex_color;"
 "out vec3 color;"
-"void main() {"
-"   color = vertex_color;"
-"   gl_position = vec4(vertex_position, 1.0);"
+"void main(){"
+"  color = vertex_color;"
+"  gl_Position = vec4(vertex_position, 1.0);"
 "}";
 
 const char* fragment_shader =
 "#version 460\n"
-"in ver3 color;"
+"in vec3 color;"
 "out vec4 frag_color;"
-"void main() {"
+"void main(){"
 "   frag_color = vec4(color, 1.0);"
 "}";
 
