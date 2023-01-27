cad8d9f73f114efc62e2b33b0a9651a6def82bed
Kirill <k.bushtets@gmail.com>
2023-01-27 19:48:58 +0200

    Added Texture2D class

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 
 #include <sstream>
 #include <fstream>
@@ -54,7 +55,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::loadShaders(const std:
 
 std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const std::string& shaderName) {
 	
-	SgaderProgramsMap::const_iterator it = m_shaderPrograms.find(shaderName);
+	ShaderProgramsMap::const_iterator it = m_shaderPrograms.find(shaderName);
 	if (it != m_shaderPrograms.end()) {
 		return it->second;
 	}
@@ -63,7 +64,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	return nullptr; 
 }
 
-void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath) {
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath) {
 	
 	int channels = 0;
 	int width = 0;
@@ -74,8 +75,22 @@ void ResourceManager::loadTexture(const std::string& textureName, const std::str
 	unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &channels, 0);
 	if (!pixels) {
 		std::cerr << "Can`t load image: " << textureName << std::endl;
-		return;
+		return nullptr;
 	}
 
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared < Renderer::Texture2D>(width, height, pixels, channels, GL_NEAREST, GL_CLAMP_TO_EDGE)).first->second;
+
 	stbi_image_free(pixels);
+
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName) {
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end()) {
+		return it->second;
+	}
+
+	std::cerr << "Can`t find the texture: " << textureName << std::endl;
+	return nullptr;
 }
