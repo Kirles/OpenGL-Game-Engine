b66544934e1d34894e2c91c0b778a395d85e0360
Kirill <k.bushtets@gmail.com>
2023-02-07 12:10:06 +0200

    Removed work tests

--- src/main.cpp
+++ src/main.cpp
@@ -19,24 +19,6 @@ extern "C" { _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001
 /*For AMD*/
 //extern "C" { _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1; }
 
-GLfloat point[] = {
-    0.0f,  50.f, 0.0f,
-    50.f, -50.f, 0.0f,
-   -50.f, -50.f, 0.0f,
-};
-
-GLfloat colors[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
-};
-
-GLfloat texCoord[] = {
-    0.5f, 1.0f,
-    1.0f, 0.0f,
-    0.0f, 0.0f
-};
-
 glm::ivec2 g_windowSize(640, 480);
 
 bool isEagle = false;
@@ -86,7 +68,7 @@ int main(int argc, char** argv) {
     std::cout << "Rendered: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(1, 1, 0, 1);
+    glClearColor(0.5, 1, 0, 1);
 
     {
         ResourceManager resourceManager(argv[0]);
@@ -101,8 +83,6 @@ int main(int argc, char** argv) {
             std::cerr << "Can`t create shader program: SpriteShader" << std::endl;
             return -1;
         }
-
-        auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/map_16x16.png");
 
         std::vector<std::string> subTexturesNames = {
             "block",
@@ -141,13 +121,14 @@ int main(int argc, char** argv) {
             "respawn4"
 
         };
+
         auto pTextureAtlas = resourceManager.loadTextureAtlas("DefaultTextureAtlas", "res/textures/map_16x16.png", std::move(subTexturesNames), 16, 16);
 
-        auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTextureAtlas", "SpriteShader", 100, 100, "block");
-        pSprite->setPosition(glm::vec2(300, 100));
+        auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTextureAtlas", "SpriteShader", 100, 100, "beton");
+        pSprite->setPosition(glm::vec2(275, 100));
 
-        auto pAnimatedSprite = resourceManager.loadAnimatedSprite("NewAnimatedSprite", "DefaultTextureAtlas", "SpriteShader", 100, 100, "beton");
-        pAnimatedSprite->setPosition(glm::vec2(300, 300));
+        auto pAnimatedSprite = resourceManager.loadAnimatedSprite("NewAnimatedSprite", "DefaultTextureAtlas", "SpriteShader", 100, 100, "water1");
+        pAnimatedSprite->setPosition(glm::vec2(275, 300));
         
         std::vector<std::pair<std::string, uint64_t>> waterState;
         waterState.emplace_back(std::make_pair<std::string, uint64_t>("water1", 1000000000));
@@ -163,52 +144,9 @@ int main(int argc, char** argv) {
 
         pAnimatedSprite->setState("waterState");
 
-        GLuint points_vbo = 0;
-        glGenBuffers(1, &points_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
-
-        GLuint colors_vbo = 0;
-        glGenBuffers(1, &colors_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-        GLuint texCoord_vbo = 0;
-        glGenBuffers(1, &texCoord_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(texCoord), texCoord, GL_STATIC_DRAW);
-
-        GLuint vao = 0;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        glEnableVertexAttribArray(2);
-        glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        pDefaultShaderProgram->use();
-        pDefaultShaderProgram->setInt("tex", 0);
-
-        glm::mat4 modelMatrix_1 = glm::mat4(1.f);
-        modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(50.f, 50.f, 0.f));
-
-        glm::mat4 modelMatrix_2 = glm::mat4(1.f);
-        modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(590.f, 50.f, 0.f));
-
         glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f, static_cast<float>(g_windowSize.y), -100.f, 100.f);
 
-        pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix);
-
         pSpriteShaderProgram->use();
-        pSpriteShaderProgram->setInt("tex", 0);
         pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 
         auto lastTime = std::chrono::high_resolution_clock::now();
@@ -229,16 +167,6 @@ int main(int argc, char** argv) {
             pAnimatedSprite->update(duration);
 
             glClear(GL_COLOR_BUFFER_BIT);
-
-            pDefaultShaderProgram->use();
-            glBindVertexArray(vao);
-            tex->bind(); 
-
-            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_1);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-
-            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
 
             pSprite->render();
 
